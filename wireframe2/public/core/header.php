3ac3abfc9ede73b65756b226b5e69aef0404cbc8
Riley de Man <rileydeman@gmail.com>
2024-06-04 14:00:55 +0200

    Added admin and wireframe2 folders
    Added admin and wireframe2 folders and added a font to the fonts folders.

---
+++ wireframe2/public/core/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul class="navbar">
+    <li><a href="#"><i class="fi fi-rr-home"></i> Home</a></li>
+    <li><a href="#"><i class="fi fi-rr-info"></i> About</a></li>
+    <li><a href="#"><i class="fi fi-rr-user"></i> Contact</a></li>
+    <li><a href="#"><i class="fi fi-rr-shopping-cart"></li>Shopping cart</a></li>
+    </ul>
+</body>
+</html>

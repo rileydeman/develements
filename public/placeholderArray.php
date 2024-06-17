58c62552a4ced6b917ccebc6dab50b6ce67b5154
Rick <144334803+8-Click@users.noreply.github.com>
2024-06-17 12:37:52 +0200

    Servicepagina design idee pittig af

---
+++ public/placeholderArray.php
@@ -0,0 +1,23 @@
+<?php 
+$diensten = [
+            [
+                "dienst" => "Tuinontwerp",
+                "img" => "/public/assets/img/tuinontwerp1.jpg",
+                "tekst" => "Een mooie tuin valt of staat bij een goed tuinontwerp. Als het op ontwerpen aankomt, maakt Hendrik Hogendijk het verschil."
+            ],
+            [
+                "dienst" => "Tuinonderhoud",
+                "img" => "/public/assets/img/tuinonderhoud1.png",
+                "tekst" => "Een mooie tuin verdient het juiste onderhoud. Immers, goed onderhoud is een essentiële voorwaarde voor een tuin die het hele jaar door mooi blijft."
+            ],
+            [
+                "dienst" => "Zwemvijver",
+                "img" => "/public/assets/img/tuinzwemvijver1.jpg",
+                "tekst" => "Zwemvijvers, is één van mjn grootste passies. En gelukkig, ze winnen aan steeds meer populariteit, waardoor ik er veel ervaring mee heb opgebouwd."
+            ],
+            [
+                "dienst" => "Tuinaanleg",
+                "img" => "/public/assets/img/tuinaanleg1.jpg",
+                "tekst" => "Het aanleggen van een tuin doen ik altijd vanuit dezelfde drive: ik wil er iets moois van maken."
+            ],
+        ];

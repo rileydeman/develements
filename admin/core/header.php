c7b3964a5ad0f8d3cf6554d74d59cdbf72796f8c
Riley de Man <rileydeman@gmail.com>
2024-06-19 14:54:36 +0200

    Start at CMS header + update logboek Riley
    Start at CMS header + update logboek Riley

--- admin/core/header.php
+++ admin/core/header.php
@@ -1,3 +1,11 @@
 <div id="container">
-    <p>Container</p>
+    <div id="searchBars">
+        <p>Search bars</p>
+    </div>
+    <div id="accountName">
+        <p>Account Naam</p>
+    </div>
+    <div id="menuIcon">
+        <img src="<?= BASEURL_CMS ?>assets/img/icons/bars-staggered.png" alt="">
+    </div>
 </div>

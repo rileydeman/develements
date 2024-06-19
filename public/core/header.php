a53da63f7f6139428aac03b30ee2ecf4d7756197
Riley de Man <rileydeman@gmail.com>
2024-06-19 13:54:00 +0200

    Header & footer fix
    Header & footer fix

--- public/core/header.php
+++ public/core/header.php
@@ -1,24 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= BASEURL; ?>public/assets/css/header.css">
-    <title>Document</title>
-</head>
-<body>
-<header>
-        <nav class="navbar">
-            <div class="logo">Hier komt logo</div>
-            <ul class="nav-links">
-                <li><a href="#"><i class="fi fi-rr-home"></i> Diensten</a></li>
-                <li><a href="#"><i class="fi fi-rr-info"></i> Over mij</a></li>
-                <li><a href="#"><i class="fi fi-rr-user"></i> Contact</a></li>
-            </ul>
-            <div class="menu-toggle" id="mobile-menu">
-                <i class="fi fi-rr-menu-burger"></i>
-            </div>
-        </nav>
-    </header>
-</body>
-</html>
+<nav class="navbar">
+    <div class="logo">Hier komt logo</div>
+    <ul class="nav-links">
+        <li><a href="#"><i class="fi fi-rr-home"></i> Diensten</a></li>
+        <li><a href="#"><i class="fi fi-rr-info"></i> Over mij</a></li>
+        <li><a href="#"><i class="fi fi-rr-user"></i> Contact</a></li>
+    </ul>
+    <div class="menu-toggle" id="mobile-menu">
+        <i class="fi fi-rr-menu-burger"></i>
+    </div>
+</nav>

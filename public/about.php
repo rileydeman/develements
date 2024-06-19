648bfc98acc14e1c1c236ba33d57ca4de4a50b88
Rick <144334803+8-Click@users.noreply.github.com>
2024-06-19 14:05:00 +0200

    COMMIT EN NIET VERANDEREN

--- public/about.php
+++ public/about.php
@@ -1,52 +1,80 @@
-<?php
-/* @var mysqli $conn */
-include('../app/db/db-conn.php');
-?>
-
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="en-AU">
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./public/assets/css/about.css">
-    <link rel="stylesheet" href="./public/assets/css/style.css">
-    <title>Document</title>
+    <meta name="viewport"content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About | siteName</title>
+
+    <link rel="icon" href="">
+
+    <!--    Linking of the css files-->
+    <link rel="stylesheet" href="/public/assets/css/fonts.css">
+    <link rel="stylesheet" href="/public/assets/css/style.css">
+    <link rel="stylesheet" href="/public/assets/css/about.css">
+
+    <!--    Linking of the javascript files, defer = reading/link the file at last-->
+    <script defer src="/public/assets/js/app.js"></script>
 </head>
 
 <body>
-<header>
-    <?php include('core/header.php');?>
-</header>
 
-<main>
-    <div id="bigContainer">
-        <?php include
-        "./placeholderArray.php";
+    <header>
+        <!--    Getting the header document-->
+        <?php include "core/header.php" ?>
+    </header>
 
+    <main>
+        <div id="bigContainer">
+            <?php include "./placeholderArray.php";
+            
 
-        foreach ($diensten as $dienstBox) {
-            ?>
-            <div class="dienstBox">
-                <div class="textBox">
-                    <h2><?= $dienstBox["dienst"]; ?></h2> </br>
-                    <p><?= $dienstBox["tekst"]; ?></p> </br>
-                    <div class="buttonBox">
-                        <button class="dienstButton">Meer info</button>
+            foreach ($diensten as $dienstBox) {
+                ?>
+                <div class="dienstBox">
+                    <div class="textBox">
+                        <h2><?= $dienstBox["dienst"]; ?></h2> </br>
+                        <p><?= $dienstBox["tekst"]; ?></p> </br>
+                        <div class="buttonBox">
+                            <button class="dienstButton">Meer info</button>
+                        </div>
+                    </div>
+                    <div class="imageContainer" style='background-image: url("http://localhost<?= $dienstBox["img"] ?>");'>
+                    </div>
+                    <div class="iconBox" style='background-image: url("http://localhost<?= $dienstBox["icon"] ?>");'>
                     </div>
                 </div>
-                <div class="imageContainer" style='background-image: url("http://localhost<?= $dienstBox["img"] ?>");'>
+                <?php
+            }
+            ?>
+
+            <div id="bigContainer2">
+                <div id="contactContainer">
+                    <div id="hendrik">
+                        <div id="hendrikImage" style='background-image: url("./public/assets/img/hendrik.jpg");'>
+                        </div>
+                        <p id="hendrikSpeech">"We streven ernaar om constant in contact te staan met onze klanten totdat
+                            de
+                            klus geklaard is. Als u vragen of speciale verzoeken heeft, stuur ons dan een bericht. Voor
+                            een
+                            vrijblijvende offerte kunt u contact met ons opnemen wanneer het u uitkomt. Wij zijn u graag
+                            van
+                            dienst!" - Hendrik Hogendijk</p>
+                    </div>
+                    <hr class="divider">
+                </div>
+                <div id="contactContainer2">
+
                 </div>
             </div>
-            <?php
-        }
-        ?>
-    </div>
-</main>
+        </div>
+    </main>
 
-<footer>
-    <?php include('core/footer.php');?>
-</footer>
+    <footer>
+        <!--    Getting the footer document-->
+        <?php include "core/footer.php" ?>
+    </footer>
 
 </body>
 
--- public/placeholderArray.php
+++ public/placeholderArray.php
@@ -3,21 +3,25 @@ $diensten = [
             [
                 "dienst" => "Tuinontwerp",
                 "img" => "/public/assets/img/tuinontwerp1.jpg",
+                "icon" => "/public/assets/img/tuinontwerp_icon.png",
                 "tekst" => "Een mooie tuin valt of staat bij een goed tuinontwerp. Als het op ontwerpen aankomt, maakt Hendrik Hogendijk het verschil."
             ],
             [
                 "dienst" => "Tuinonderhoud",
                 "img" => "/public/assets/img/tuinonderhoud1.png",
+                "icon" => "/public/assets/img/tuinonderhoud_icon.png",
                 "tekst" => "Een mooie tuin verdient het juiste onderhoud. Immers, goed onderhoud is een essentiële voorwaarde voor een tuin die het hele jaar door mooi blijft."
             ],
             [
                 "dienst" => "Zwemvijver",
                 "img" => "/public/assets/img/tuinzwemvijver1.jpg",
+                "icon" => "/public/assets/img/zwemvijver_icon.png",
                 "tekst" => "Zwemvijvers, is één van mjn grootste passies. En gelukkig, ze winnen aan steeds meer populariteit, waardoor ik er veel ervaring mee heb opgebouwd."
             ],
             [
                 "dienst" => "Tuinaanleg",
                 "img" => "/public/assets/img/tuinaanleg1.jpg",
+                "icon" => "/public/assets/img/tuinaanleg_icon.png",
                 "tekst" => "Het aanleggen van een tuin doen ik altijd vanuit dezelfde drive: ik wil er iets moois van maken."
             ],
         ];

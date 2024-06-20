0b16df4257fcbfb184d0fd241d8afad74f219a5e
Riley de Man <rileydeman@gmail.com>
2024-06-20 12:03:42 +0200

    Linking some css files
    Linking some css files

--- public/contactForm.php
+++ public/contactForm.php
@@ -2,11 +2,22 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Us</title>
-    <link rel="stylesheet" href="./public/assets/css/contact.css">
-    <link rel="stylesheet" href="./public/assets/css/style.css">
-    <link rel="stylesheet" href="./public/assets/css/fonts.css">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact | siteName</title>
+
+    <link rel="icon" href="">
+
+    <!--    Linking of the css files-->
+    <link rel="stylesheet" href="/public/assets/css/fonts.css">
+    <link rel="stylesheet" href="/public/assets/css/style.css">
+    <link rel="stylesheet" href="/public/assets/css/header.css">
+    <link rel="stylesheet" href="/public/assets/css/footer.css">
+    <link rel="stylesheet" href="/public/assets/css/contact.css">
+
+    <!--    Linking of the javascript files, defer = reading/link the file at last-->
+    <script defer src="/public/assets/js/app.js"></script>
 </head>
 <body>
 

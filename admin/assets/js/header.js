90b08573fc5334c2fd95e23a406bd5414ed42db1
Riley de Man <rileydeman@gmail.com>
2024-06-24 11:22:37 +0200

    CMS menu better working in responsive
    CMS menu better working in responsive

--- admin/assets/js/header.js
+++ admin/assets/js/header.js
@@ -10,18 +10,37 @@ for (let i = 0; i < forms.length; i++) {
 
 let menuIcon = document.getElementById("menuIconImg");
 let menu = document.querySelector("aside");
+let menuOpen = false;
 
 menuIcon.addEventListener("click", (e) => {
     menu.style.right = 0;
+    menuOpen = !menuOpen;
 });
 
 let menuCloseIcon = document.getElementById("exitIcon");
 
 menuCloseIcon.addEventListener("click", (e) => {
-    if (window.innerWidth < 768) {
+    if (windowWidth < 768) {
         menu.style.right = "-80%";
-    } else if (window.innerWidth > 1279) {
+    } else if (windowWidth < 1279) {
         menu.style.right = "-50%";
     }
 
+    menuOpen = !menuOpen;
 })
+
+let windowWidth = window.innerWidth;
+
+setInterval(setWidth, 1)
+
+function setWidth() {
+    windowWidth = window.innerWidth;
+
+    if (!menuOpen) {
+        if (windowWidth < 768) {
+            menu.style.right = "-80%";
+        } else if (windowWidth < 1279) {
+            menu.style.right = "-50%";
+        }
+    }
+}
